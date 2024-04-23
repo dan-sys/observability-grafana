57366c0e7eceffc6af183e0db0a21e4d3b28b6a2
dan-sys <adeniyidaniel3@gmail.com>
2024-04-23 22:35:17 +0200

    setupController fix

--- main.tf
+++ main.tf
@@ -21,20 +21,29 @@ terraform {
 }
 
 
+data "aws_key_pair" "ansible_labsetup_kp" {
+  key_name           = "ansible_labsetup_kp"
+}
+
+
 resource "aws_s3_bucket" "terraform_state_bucket" {
   bucket = "tfstate-bucket-observe-with-grafana"
-  force_destroy = true
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 # vpc 
-resource "aws_default_vpc" "VPC_obs" {
+resource "aws_default_vpc" "default_vpc" {
 
   tags = {
     Name = "default vpc"
   }
 }
+# use data source to get all avalablility zones in region
+data "aws_availability_zones" "available_zones" {}
 
-# create default subnet if one does not exit
+## create default subnet if one does not exit
 resource "aws_default_subnet" "default_az1" {
   availability_zone = data.aws_availability_zones.available_zones.names[0]
 
@@ -52,7 +61,7 @@ resource "aws_default_subnet" "default_az2" {
 
 # create security group for the ansible machine
 resource "aws_security_group" "ec2_sg" {
-  name        = "ec2 security group"
+  name        = "ec2 security group controller"
   description = "allow access on ports 80 and 22"
   vpc_id      =  aws_default_vpc.default_vpc.id
 
@@ -108,38 +117,44 @@ resource "aws_security_group" "ec2_sg_Nodes" {
   }
 }
 
-data "aws_ami" "amazon_linux_2" {
+data "aws_ami" "amazon_linux_2023" {
   most_recent = true
   owners      = ["amazon"]
 
   filter {
-    name   = "owner-alias"
-    values = ["amazon"]
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
   }
 
   filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*"]
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
 resource "aws_instance" "controller_instance" {
-  ami           = data.aws_ami.amazon_linux_2.id # replace with your AMI ID
+  ami           = data.aws_ami.amazon_linux_2023.id # replace with your AMI ID
   instance_type = "t2.micro"
   subnet_id     = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-
+  key_name = data.aws_key_pair.ansible_labsetup_kp.key_name
+  user_data = file("setupController.sh")
   tags = {
     Name = "controller_instance"
   }
 }
 
 resource "aws_instance" "node_instances" {
-  ami           = data.aws_ami.amazon_linux_2.id # replace with your AMI ID
+  ami           = data.aws_ami.amazon_linux_2023.id # replace with your AMI ID
   instance_type = "t2.micro"
   subnet_id     = aws_default_subnet.default_az2.id
   vpc_security_group_ids = [aws_security_group.ec2_sg_Nodes.id]
   count = 2
+  #key_name = data.aws_key_pair.ansible_labsetup_kp.key_name
 
   tags = {
     Name = "node_instances"
@@ -150,9 +165,9 @@ output "ec2_controller_public_ipv4" {
   value = aws_instance.controller_instance.public_ip
 }
 
-output "ec2_nodes_public_ipv4_1" {
-  value = aws_instance.node_instances[0].public_ip
+output "ec2_nodes_private_ipv4_1" {
+  value = aws_instance.node_instances[0].private_ip
 }
-output "ec2_nodes_public_ipv4_2" {
-  value = aws_instance.node_instances[1].public_ip
+output "ec2_nodes_private_ipv4_2" {
+  value = aws_instance.node_instances[1].private_ip
 }
---
+++ setupController.sh
@@ -0,0 +1,32 @@
+!#/usr/bin/env bash
+
+#fail if any error encountered
+set -eu
+
+#change hostname
+sudo hostnamectl set-hostname controller
+
+sudo yum update -y
+sudo amazon-linux-extras install ansible2 -y
+
+echo ansible --version
+
+ssh-keygen -t rsa -b 2048 -f /home/ec2-user/.ssh/id_rsa -q -P ""
+
+
+cd .ssh
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
